1250a2936c5bbe10c28c6036e738196842d6c6e0
titrxw <1120309488@qq.com>
2021-01-07 23:15:53 +0800

    init

--- main/test.go
+++ main/test.go
@@ -5,11 +5,11 @@ import (
 	"github.com/titrxw/gin-router/Src"
 )
 
-func test() {
+func routeHandler() {
 
 }
 
-func main() {
+func initRoute() Src.RouteGroup {
 	group := Src.RouteGroup{
 		"",
 		"",
@@ -20,23 +20,43 @@ func main() {
 		make(Src.RouteMiddlewares, 0),
 	}
 
-	group.Get("test", test)
-	group.Middleware(test).Group("/er", func(group *Src.RouteGroup) {
-		group.Post("/erdfgf", test)
-		group.Group("/fdg", func(group *Src.RouteGroup) {
-			group.Delete("/fgh", test)
-		})
-		group.Middleware(test).Group("/hgjk", func(group *Src.RouteGroup) {
-			group.Head("/he", test)
-		})
-		group.Post("/erdfgferw", test)
-	})
+	return group
+}
 
-	fmt.Println((group.RouteCollector.RouteMethodMap[Src.MethodHead]))
+func TestBase() {
+	group := initRoute()
+
+	group.Get("test", routeHandler)
+	group.Middleware(routeHandler).Group("/er", func(group *Src.RouteGroup) {
+		group.Post("/erdfgf", routeHandler)
+		group.Group("/fdg", func(group *Src.RouteGroup) {
+			group.Delete("/fgh", routeHandler)
+		})
+		group.Middleware(routeHandler).Group("/hgjk", func(group *Src.RouteGroup) {
+			group.Head("/he", routeHandler)
+		})
+		group.Post("/erdfgferw", routeHandler)
+	})
+	group.Name("test").Post("/test", routeHandler)
 
 	routeDispatcher := Src.RouteDispatcher{
 		group.RouteCollector,
 	}
-	route := routeDispatcher.Dispatcher(Src.MethodPost, "/er/erdfgferw")
-	fmt.Println(route)
+
+	route := routeDispatcher.Dispatcher(Src.MethodPost, "/test")
+
+	if route.GetName() == "test" {
+		fmt.Println("route /test found")
+	}
+}
+
+func main() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	TestBase()
 }
